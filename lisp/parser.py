6d2e8d3f2267a878638d3fb7262d96b9e81bda53
ktoyabe <ktoyabe@gmail.com>
2022-11-10 03:45:06 +0000

    parser supports multi-lines

--- lisp/parser.py
+++ lisp/parser.py
@@ -9,25 +9,38 @@ class ParseError(Exception):
         super().__init__("Parse error: {}".format(err))
 
 
+def _parse_list(tokens: List[token.Token]):
+    List[lobject.Object]
+
+    t = tokens.pop(-1)
+    if t != token.LParen:
+        raise ParseError("Expected LParen, found {}".format(t))
+
+    objects: List[lobject.Object] = []
+    while len(tokens) != 0:
+        t = tokens.pop(-1)
+
+        if t.token_type == token.TokenType.INTEGER:
+            objects.append(lobject.Integer(t.i))
+        elif t.token_type == token.TokenType.SYMBOL:
+            objects.append(lobject.Symbol(t.s))
+        elif t.token_type == token.TokenType.LPAREN:
+            tokens.append(token.LParen)
+            sub_list = _parse_list(tokens)
+            objects.append(sub_list)
+        elif t.token_type == token.TokenType.RPAREN:
+            return lobject.List(objects)
+
+    return lobject.List(objects)
+
+
 def parse(tokens: List[token.Token]):
     List[lobject.Object]
 
     if len(tokens) == 0:
         raise ParseError("tokens are empty.")
 
-    t = tokens.pop(0)
-    if t != token.LParen:
-        raise ParseError("Expected LParen, found {}".format(t))
+    ts = tokens.copy()
+    ts.reverse()
 
-    objects: List[lobject.Object] = []
-    while len(tokens) != 0:
-        t = tokens.pop(0)
-
-        if t.token_type == token.TokenType.INTEGER:
-            objects.append(lobject.Integer(t.i))
-        elif t.token_type == token.TokenType.SYMBOL:
-            objects.append(lobject.Symbol(t.s))
-        elif t.token_type == token.TokenType.RPAREN:
-            return lobject.List(objects)
-
-    return lobject.List(objects)
+    return _parse_list(ts)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,12 +5,43 @@ def test_add():
     tokens = token.tokenize("(+ 1 2)")
     l = parser.parse(tokens)
 
-    print("".format(l))
-    assert type(l) == lobject.List
     assert l == lobject.List(
         [
             lobject.Symbol("+"),
             lobject.Integer(1),
             lobject.Integer(2),
+        ]
+    )
+
+
+def test_area_of_circle():
+    programs = """(
+        (define r 10)
+        (* pi (* r r))
+    )"""
+    tokens = token.tokenize(programs)
+    l = parser.parse(tokens)
+    assert l == lobject.List(
+        [
+            lobject.List(
+                [
+                    lobject.Symbol("define"),
+                    lobject.Symbol("r"),
+                    lobject.Integer(10),
+                ]
+            ),
+            lobject.List(
+                [
+                    lobject.Symbol("*"),
+                    lobject.Symbol("pi"),
+                    lobject.List(
+                        [
+                            lobject.Symbol("*"),
+                            lobject.Symbol("r"),
+                            lobject.Symbol("r"),
+                        ]
+                    ),
+                ]
+            ),
         ]
     )
